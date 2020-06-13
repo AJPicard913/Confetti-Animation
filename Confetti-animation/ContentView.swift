60c78f735ad2b35b8e1b44c5fde377f330c09d37
ajpicard913 <Contact@saucedesigns.io>
2020-06-13 00:49:16 -0400

    Initial Commit

---
+++ Confetti-animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Confetti-animation
+//
+//  Created by AJ Picard on 6/13/20.
+//  Copyright © 2020 AJ Picard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
